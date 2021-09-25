6437fe61c654f26af89d1ce8659bca2b309ea088
Viktor Bengtsson <vibe16@student.bth.se>
2021-09-25 14:57:37 +0200

    Added assignment2

---
+++ assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Selection and iteration in C#";
+            SelectionAndIteration warmupObj = new SelectionAndIteration();
+
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadLine();
+
+            Console.Title = "Fahrenheit and Ceslsius Converter";
+            /* TemperatureConverter converter = new TemperatureConverter(); */
+
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment2/TemperatureConverter.cs
@@ -0,0 +1,10 @@
+using System;
+
+class TemperatureConverter
+{
+
+  public TemperatureConverter()
+  {
+
+  }
+}
